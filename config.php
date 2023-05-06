74557677799a13a20e9d286211a4699204fd0d32
BuildTools <unconfigured@null.spigotmc.org>
2023-05-06 17:59:08 +0200

    commit

--- config.php
+++ config.php
@@ -15,4 +15,7 @@ $link = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
 ?>
